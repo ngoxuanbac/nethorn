c6f4cf351a100b067739b3a99b01830a697bba7d
bacdepzai <ngobac20031016@gmail.com>
2025-07-01 19:59:37 +0700

    update

--- src/features/constants/elevenLabsParam.ts
+++ src/features/constants/elevenLabsParam.ts
@@ -2,7 +2,6 @@ export type ElevenLabsParam = {
   voiceId: string;
 };
 
-
 export const DEFAULT_ELEVEN_LABS_PARAM: ElevenLabsParam = {
-  voiceId: "MF3mGyEYCl7XYWbV9V6O", // Elli
+  voiceId: "iSFxP4Z6YNcx9OXl62Ic",
 } as const;
